9a68ccbaa4e40e9fce93ef923aaceaff0084b81d
ElDuartte <jf@duartte.co>
2025-07-01 00:34:46 +0200

    wip home styles and structure, missing api implementation to do the filter correctly

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,22 +1,57 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { apiTest } from '../../services/api';
 
 function Home() {
-  const api = apiTest;
+  const [search, setSearch] = useState('');
+
+  // TODO change this to work with API filtering
+  // needs to search in real time with name and brand
+  // needs to show the user the number of results found
+
+  // TODO filter the first 20 phones in the grid
+
+  const filteredProducts = apiTest.filter(
+    (product) =>
+      product.brand.toLowerCase().includes(search.toLowerCase()) ||
+      product.name.toLowerCase().includes(search.toLowerCase())
+  );
+
   return (
-    <div className="product-card product-card--home">
-      {api.map((product, key) => (
-        <div className="product-card__body" key={key}>
-          <img className="product-card__image" src={product.imageUrl} alt="" />
-          <div className="product-card__text">
-            <p className="product-card__brand">{product.brand}</p>
-            <div className="product-card__model-price">
-              <h2 className="product-card__model">{product.name}</h2>
-              <h2 className="product-card__price">${product.basePrice}</h2>
+    <div className="home-page">
+      <div className="search-container">
+        <input
+          id="search-input"
+          type="text"
+          placeholder="Search for a smartphone..."
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+          className="search-input"
+        />
+
+        <p className="results-count">
+          {filteredProducts.length} result
+          {filteredProducts.length !== 1 ? 's' : ''} found
+        </p>
+      </div>
+
+      <div className="product-card product-card--home">
+        {filteredProducts.map((product, key) => (
+          <div className="product-card__body" key={key}>
+            <img
+              className="product-card__image"
+              src={product.imageUrl}
+              alt=""
+            />
+            <div className="product-card__text">
+              <p className="product-card__brand">{product.brand}</p>
+              <div className="product-card__model-price">
+                <h2 className="product-card__model">{product.name}</h2>
+                <h2 className="product-card__price">${product.basePrice}</h2>
+              </div>
             </div>
           </div>
-        </div>
-      ))}
+        ))}
+      </div>
     </div>
   );
 }
