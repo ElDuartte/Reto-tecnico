d895642ae0853a5093ea0c59c4171c70e10f553d
ElDuartte <jf@duartte.co>
2025-07-01 13:02:03 +0200

    Gitignore, and basic two get endpoints

---
+++ src/services/api.js
@@ -0,0 +1,40 @@
+// Add create a .env file and add the two variables
+const BASE_URL = import.meta.env.VITE_API_BASE_URL;
+const API_KEY = import.meta.env.VITE_API_KEY;
+
+// GET all the phones
+export async function getProducts(endpoint, options = {}) {
+  const url = `${BASE_URL}${endpoint}`;
+  try {
+    const response = await fetch(url, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        'x-api-key': API_KEY,
+        ...(options.headers || {}),
+      },
+      ...options,
+    });
+
+    if (!response.ok) {
+      const errorData = await response.json().catch(() => ({}));
+      const message =
+        errorData.message || `HTTP error! status: ${response.status}`;
+      throw new Error(message);
+    }
+
+    return await response.json();
+  } catch (error) {
+    console.error('API GET Error:', error);
+    throw error;
+  }
+}
+
+// GET the specific product by ID
+export async function getProductById(id, options = {}) {
+  console.log('ID:' + id);
+  if (!id) {
+    throw new Error('Product ID is required');
+  }
+  return getProducts(`/products/${id}`, options);
+}
