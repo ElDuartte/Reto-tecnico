33b438bafc96141b457b7ceac93b60a915cd5668
ElDuartte <jf@duartte.co>
2025-07-02 23:37:42 +0200

    adding logic to first load nothing selected, and styles for button, wip

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
+import { useParams, Link } from 'react-router-dom';
 import { getProductById } from '../services/api';
 
 function Product() {
@@ -9,6 +9,8 @@ function Product() {
   const [selectedStorage, setSelectedStorage] = useState(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
+  const [colorTouched, setColorTouched] = useState(false);
+  const [storageTouched, setStorageTouched] = useState(false);
 
   useEffect(() => {
     async function fetchProduct() {
@@ -32,116 +34,134 @@ function Product() {
   if (!product) return <div>No product found</div>;
 
   return (
-    <div className="container">
-      <section className="product-detail">
-        <div className="product-detail__left">
-          <img
-            src={selectedColor.imageUrl}
-            alt={selectedColor.name}
-            className="product-detail__image"
-          />
-        </div>
-
-        <div className="product-detail__right">
-          <h1 className='product-detail__title'>
-            {product.brand} {product.name}
-          </h1>
-          {/* <p>{product.description}</p> */}
-
-          <div className="product-detail__price">
-            {selectedStorage.price} EUR
+    <>
+      <section className="container">
+        <Link to="/">&lt;  Back</Link>
+      </section>
+      <div className="container__product">
+        <section className="product-detail">
+          <div className="product-detail__left">
+            <img
+              src={selectedColor.imageUrl}
+              alt={selectedColor.name}
+              className="product-detail__image"
+            />
           </div>
 
-          <div className="product-detail__storage">
-            <h4>STORAGE ¿HOW MUCH SPACE DO YOU NEED?</h4>
-            <div className="storage-options">
-              {product.storageOptions.map((option) => (
-                <button
-                  key={option.capacity}
-                  onClick={() => setSelectedStorage(option)}
-                  className={`storage-button ${
-                    selectedStorage.capacity === option.capacity
-                      ? 'selected'
-                      : ''
-                  }`}
-                >
-                  {option.capacity}
-                </button>
-              ))}
+          <div className="product-detail__right">
+            <h1 className="product-detail__title">
+              {product.brand} {product.name}
+            </h1>
+            {/* <p>{product.description}</p> */}
+
+            <div className="product-detail__price">
+              {!storageTouched && 'From '}
+              {selectedStorage.price} EUR
             </div>
-          </div>
 
-          <div className="product-detail__colors">
-            <h4>COLOR. PICK YOUR FAVOURITE.</h4>
-            <div className="color-options__container">
-              <div className="color-options">
-                {product.colorOptions.map((color) => (
+            <div className="product-detail__storage">
+              <h4>STORAGE ¿HOW MUCH SPACE DO YOU NEED?</h4>
+              <div className="storage-options">
+                {product.storageOptions.map((option) => (
                   <button
-                    key={color.name}
-                    onClick={() => setSelectedColor(color)}
-                    className={`color-circle ${
-                      selectedColor.name === color.name ? 'selected' : ''
+                    key={option.capacity}
+                    onClick={() => {
+                      setSelectedStorage(option);
+                      setStorageTouched(true);
+                    }}
+                    className={`storage-button ${
+                      selectedStorage.capacity === option.capacity
+                        ? 'selected'
+                        : ''
                     }`}
-                    style={{ backgroundColor: color.hexCode }}
-                    title={color.name}
-                  />
+                  >
+                    {option.capacity}
+                  </button>
                 ))}
               </div>
-              <p>{selectedColor.name}</p>
+            </div>
+
+            <div className="product-detail__colors">
+              <h4>COLOR. PICK YOUR FAVOURITE.</h4>
+              <div className="color-options__container">
+                <div className="color-options">
+                  {product.colorOptions.map((color) => (
+                    <button
+                      key={color.name}
+                      onClick={() => {
+                        setSelectedColor(color);
+                        setColorTouched(true);
+                      }}
+                      className={`color-square ${
+                        selectedColor.name === color.name ? 'selected' : ''
+                      }`}
+                      style={{ backgroundColor: color.hexCode }}
+                      title={color.name}
+                    />
+                  ))}
+                </div>
+                <p>{selectedColor.name}</p>
+              </div>
+            </div>
+            <button
+              className="cart__button"
+              disabled={!(colorTouched && storageTouched)}
+            >
+              Add To Cart
+            </button>
+          </div>
+        </section>
+        <section className="product-specs">
+          <h2 className="specs-title">Specifications</h2>
+          <div className="specs-grid">
+            <div className="spec-row">
+              <span className="spec-key">BRAND</span>
+              <span>{product.brand}</span>
+            </div>
+            <div className="spec-row">
+              <span className="spec-key">NAME</span>
+              <span>{product.name}</span>
+            </div>
+            <div className="spec-row">
+              <span className="spec-key">DESCRIPTION</span>
+              <span>{product.description}</span>
+            </div>
+            <div className="spec-row">
+              <span className="spec-key">SCREEN</span>
+              <span>{product.specs.screen}</span>
+            </div>
+            <div className="spec-row">
+              <span className="spec-key">RESOLUTION</span>
+              <span>{product.specs.resolution}</span>
+            </div>
+            <div className="spec-row">
+              <span className="spec-key">PROCESSOR</span>
+              <span>{product.specs.processor}</span>
+            </div>
+            <div className="spec-row">
+              <span className="spec-key">MAIN CAMERA</span>
+              <span>{product.specs.mainCamera}</span>
+            </div>
+            <div className="spec-row">
+              <span className="spec-key">SELFIE CAMERA</span>
+              <span>{product.specs.selfieCamera}</span>
+            </div>
+            <div className="spec-row">
+              <span className="spec-key">BATTERY</span>
+              <span>{product.specs.battery}</span>
+            </div>
+            <div className="spec-row">
+              <span className="spec-key">OS</span>
+              <span>{product.specs.os}</span>
+            </div>
+            <div className="spec-row">
+              <span className="spec-key">SCREEN REFRESH RATE</span>
+              <span>{product.specs.screenRefreshRate}</span>
             </div>
           </div>
-        </div>
-      </section>
-      <section className="product-specs">
-        <h2 className="specs-title">Specifications</h2>
-        <div className="specs-grid">
-          <div className="spec-row">
-            <span className="spec-key">BRAND</span>
-            <span>{product.brand}</span>
-          </div>
-          <div className="spec-row">
-            <span className="spec-key">NAME</span>
-            <span>{product.name}</span>
-          </div>
-          <div className="spec-row">
-            <span className="spec-key">DESCRIPTION</span>
-            <span>{product.description}</span>
-          </div>
-          <div className="spec-row">
-            <span className="spec-key">SCREEN</span>
-            <span>{product.specs.screen}</span>
-          </div>
-          <div className="spec-row">
-            <span className="spec-key">RESOLUTION</span>
-            <span>{product.specs.resolution}</span>
-          </div>
-          <div className="spec-row">
-            <span className="spec-key">PROCESSOR</span>
-            <span>{product.specs.processor}</span>
-          </div>
-          <div className="spec-row">
-            <span className="spec-key">MAIN CAMERA</span>
-            <span>{product.specs.mainCamera}</span>
-          </div>
-          <div className="spec-row">
-            <span className="spec-key">SELFIE CAMERA</span>
-            <span>{product.specs.selfieCamera}</span>
-          </div>
-          <div className="spec-row">
-            <span className="spec-key">BATTERY</span>
-            <span>{product.specs.battery}</span>
-          </div>
-          <div className="spec-row">
-            <span className="spec-key">OS</span>
-            <span>{product.specs.os}</span>
-          </div>
-          <div className="spec-row">
-            <span className="spec-key">SCREEN REFRESH RATE</span>
-            <span>{product.specs.screenRefreshRate}</span>
-          </div>
-        </div>
-      </section>
-    </div>
+        </section>
+      </div>
+    </>
   );
 }
 
