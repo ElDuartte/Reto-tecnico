78f03ff39b3b577ff0196309de12beca81f13fb0
ElDuartte <jf@duartte.co>
2025-07-03 21:30:25 +0200

    wip, cart working, but ugly code

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,9 +1,11 @@
 import { useEffect, useState } from 'react';
 import { useCartCount } from '../hooks/useCartCount';
-import { getCart, removeFromCart } from '../utils/cart'; // adjust path
+import { Link } from 'react-router-dom';
+import { getCart, removeFromCart } from '../utils/cart';
 
 function Cart() {
   const [cart, setCart] = useState([]);
+  const totalPrice = cart.reduce((sum, item) => sum + Number(item.price), 0);
 
   useEffect(() => {
     setCart(getCart());
@@ -16,12 +18,19 @@ function Cart() {
     return () => window.removeEventListener('cartUpdated', handleCartChange);
   }, []);
 
+  const [isMobile, setIsMobile] = useState(window.innerWidth < 600);
+
+  useEffect(() => {
+    const handleResize = () => setIsMobile(window.innerWidth < 600);
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
   const handleDelete = (id) => {
     removeFromCart(id);
   };
 
   const cartCount = useCartCount();
-
 
   console.log(cartCount);
 
@@ -30,25 +39,54 @@ function Cart() {
       <h2>Cart ({cartCount})</h2>
       <div className="container-card__details">
         {cart.map((item) => (
-        <div className="card__details" key={item.id}>
-          <img src={item.imageUrl} alt={item.name} className="card__image" />
-          <div className="card-container__details">
-            <div className="card-container__text">
-              <p className="card__text">
-                {item.brand} {item.name}
-              </p>
-              <p className="card__text">
-                {item.storage} | {item.color}
-              </p>
-              <p className="card__text card-text__price">{item.price} EUR</p>
+          <div className="card__details" key={item.id}>
+            <img src={item.imageUrl} alt={item.name} className="card__image" />
+            <div className="card-container__details">
+              <div className="card-container__text">
+                <p className="card__text">
+                  {item.brand} {item.name}
+                </p>
+                <p className="card__text">
+                  {item.storage} | {item.color}
+                </p>
+                <p className="card__text card-text__price">{item.price} EUR</p>
+              </div>
+              <a
+                onClick={() => handleDelete(item.id)}
+                className="button__delete"
+              >
+                Remove
+              </a>
             </div>
-            <a onClick={() => handleDelete(item.id)} className="button__delete">
-              Remove
-            </a>
           </div>
-        </div>
         ))}
       </div>
+      <section className="container__total">
+        {isMobile ? (
+          <>
+            <div className="cart-total-wrapper">
+              <p className="cart-description__total">TOTAL </p>{' '}
+              <p className="cart-description__price">{totalPrice} EUR</p>
+            </div>
+            <div className="cart-buttons-wrapper">
+              <Link to="/" className="cart-button cart-button__continue">
+                CONTINUE SHOPPING
+              </Link>
+              <a className="cart-button">PAY</a>
+            </div>
+          </>
+        ) : (
+          <>
+            <Link to="/" className="cart-button cart-button__continue">
+              CONTINUE SHOPPING
+            </Link>
+            <div className="container__price-pay">
+              <p className="cart__total">TOTAL {totalPrice} EUR</p>
+              <a className="cart-button">PAY</a>
+            </div>
+          </>
+        )}
+      </section>
     </div>
   );
 }
