d867c283864f46814e9ba82abedf1e73c31b2f21
ElDuartte <jf@duartte.co>
2025-07-03 21:29:04 +0200

    FadeInOnChange scss and typo

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,22 @@
-import { Routes, Route, Link } from 'react-router-dom';
+import { Routes, Route, Link, useLocation } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
 import Product from './pages/Product';
 import Cart from './pages/Cart';
+import FadeInOnChange from './components/FadeInOnChange';
 
 function App() {
+  const location = useLocation();
   return (
     <>
       <Navbar />
-
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/product/:id" element={<Product />} />
-        <Route path="/cart" element={<Cart />} />
-      </Routes>
+      <FadeInOnChange key={location.pathname}>
+        <Routes location={location} key={location.pathname}>
+          <Route path="/" element={<Home />} />
+          <Route path="/product/:id" element={<Product />} />
+          <Route path="/cart" element={<Cart />} />
+        </Routes>
+      </FadeInOnChange>
 
       <section className="footer"></section>
     </>
