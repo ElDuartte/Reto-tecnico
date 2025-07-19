41661cc3470beb6494b58d9d05b0452bdf5cf37e
juan felipe <jf@duartte.co>
2025-07-19 19:53:50 +0200

    starting adding apollo, graphql to frontend WIP

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,11 +1,41 @@
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import './index.scss';
 
+// 1. Crear el link HTTP
+const httpLink = createHttpLink({
+  uri: `${import.meta.env.VITE_API_BASE_URL}/graphql`,
+});
+
+// 2. Inyectar la API KEY en el header
+const authLink = setContext((_, { headers }) => {
+  return {
+    headers: {
+      ...headers,
+      Authorization: import.meta.env.VITE_API_KEY,
+    },
+  };
+});
+
+// 3. Crear el Apollo Client con auth
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <ApolloProvider client={client}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </ApolloProvider>
 );
