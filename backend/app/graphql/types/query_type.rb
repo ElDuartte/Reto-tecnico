0c8a573b75440e6922aed951c0f546cb2c812188
juan felipe <jf@duartte.co>
2025-07-23 22:15:37 +0200

    fixing the indentation of query_type.rb

--- backend/app/graphql/types/query_type.rb
+++ backend/app/graphql/types/query_type.rb
@@ -8,45 +8,44 @@ module Types
     end
 
     def pokemons(limit:, offset:)
-  query = <<~GRAPHQL
-    {
-      pokemon_v2_pokemon(limit: #{limit}, offset: #{offset}) {
-        id
-        name
-        pokemon_v2_pokemontypes {
-          pokemon_v2_type {
-            name
+    query = <<~GRAPHQL
+      {
+        pokemon_v2_pokemon(limit: #{limit}, offset: #{offset}) {
+          id
+          name
+          pokemon_v2_pokemontypes {
+            pokemon_v2_type {
+              name
+            }
           }
-        }
-        pokemon_v2_pokemonstats {
-          base_stat
-          pokemon_v2_stat {
-            name
+          pokemon_v2_pokemonstats {
+            base_stat
+            pokemon_v2_stat {
+              name
+            }
           }
         }
       }
-    }
-  GRAPHQL
+    GRAPHQL
 
-  response = HTTParty.post("https://beta.pokeapi.co/graphql/v1beta",
-    headers: { "Content-Type" => "application/json" },
-    body: { query: query }.to_json
-  )
+    response = HTTParty.post("https://beta.pokeapi.co/graphql/v1beta",
+      headers: { "Content-Type" => "application/json" },
+      body: { query: query }.to_json
+    )
 
-  raw_pokemons = response.dig("data", "pokemon_v2_pokemon")
+    raw_pokemons = response.dig("data", "pokemon_v2_pokemon")
 
-  raw_pokemons.map do |p|
-    stats = p["pokemon_v2_pokemonstats"]
-    {
-      id: p["id"],
-      name: p["name"],
-      image_url: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/#{p["id"]}.png",
-      types: p["pokemon_v2_pokemontypes"].map { |t| t["pokemon_v2_type"]["name"] },
-      hp: stats.find { |s| s["pokemon_v2_stat"]["name"] == "hp" }&.dig("base_stat"),
-      attack: stats.find { |s| s["pokemon_v2_stat"]["name"] == "attack" }&.dig("base_stat")
-    }
-  end
-end
-
+      raw_pokemons.map do |p|
+        stats = p["pokemon_v2_pokemonstats"]
+        {
+          id: p["id"],
+          name: p["name"],
+          image_url: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/#{p["id"]}.png",
+          types: p["pokemon_v2_pokemontypes"].map { |t| t["pokemon_v2_type"]["name"] },
+          hp: stats.find { |s| s["pokemon_v2_stat"]["name"] == "hp" }&.dig("base_stat"),
+          attack: stats.find { |s| s["pokemon_v2_stat"]["name"] == "attack" }&.dig("base_stat")
+        }
+      end
+    end
   end
 end
